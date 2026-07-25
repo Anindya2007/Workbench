65b5a40842d6ffc5e0827fa979066d539ce46703
Anindya Sarkar <sanindya710@gmail.com>
2026-07-26 00:01:48 +0530

    The final contact page with all links added

--- web/src/app/Contact/Contact.jsx
+++ web/src/app/Contact/Contact.jsx
@@ -1,9 +1,49 @@
+import AppWindow from '../../components/window/AppWindow.jsx';
+import ContactWallpaper from '../../assets/ContactWallpaper.png';
+import ContactBox from './ContactBox.jsx';
+import { FaGithub, FaLinkedin, FaEnvelope, FaInstagram } from 'react-icons/fa';
 
+export default function Contact() {
+    const contacts = [
+        {
+            icon: FaGithub,
+            name: 'Github',
+            link: 'https://github.com/Anindya2007'
+        },
+        {
+            icon: FaLinkedin,
+            name: 'Linkedin',
+            link: 'https://www.linkedin.com/in/anindya-sarkar2007/'
+        },
+        {
+            icon: FaEnvelope,
+            name: 'Email',
+            link: 'anindya.s25058@nst.rishihood.edu.in'
+        },
+        {
+            icon: FaInstagram,
+            name: 'Instagram',
+            link: 'https://www.instagram.com/ani03610/'
+        }
+    ];
 
-export default function About() {
-    return(
-        <div className='w-full h-full '>
-            
-        </div>
+    return (
+        <AppWindow Image={ContactWallpaper} name='Contact'>
+
+            <div className='w-full h-full flex flex-col items-center gap-4 p-5'>
+
+                <h1 className='text-5xl text-white'>Let's <span className='text-indigo-400'>Conntect</span></h1>
+                <hr className='text-indigo-500 w-20 font-extrabold'></hr>
+                <p className='text-indigo-50 w-[60%] text-[18px] font-light'>I'm open to internships , open-source contributions, freelance opportunities and collaborations.</p>
+
+                <div className='flex items-center justify-center gap-10 w-full h-full'>
+                {contacts.map((data,index)=>{
+                    return <ContactBox key={index} name={data.name} icon={data.icon} link={data.link} />
+                })}
+                </div>
+
+            </div>
+
+        </AppWindow>
     )
-}
+};
--- web/src/components/window/AppWindow.jsx
+++ web/src/components/window/AppWindow.jsx
@@ -1,8 +1,9 @@
 import Header from './AppWindowHeader.jsx';
 
-export default function AppWindow({children, name}){
+export default function AppWindow({children, name,Image=''}){
     return(
-    <div className='w-[810px] h-[520px] absolute left-[18%] top-[15%] rounded-xl border-2 border-white/27 overflow-hidden z-30'>
+    <div className='w-[810px] h-[520px] absolute left-[18%] top-[15%] rounded-xl border-3 border-white/35 overflow-hidden z-30'
+        style={{backgroundImage:`url(${Image})`,backgroundSize:'cover',backgroundPosition:'center'}}>
         <Header name={name} />
         {children}
     </div>
