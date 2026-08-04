77583ef35815bf57f785a55792cc8b55927624b1
Anindya Sarkar <sanindya710@gmail.com>
2026-08-04 20:41:45 +0530

    About app intro

--- web/src/app/About/About.jsx
+++ web/src/app/About/About.jsx
@@ -1,9 +1,18 @@
+import AppWindowHeader from "../../components/window/AppWindowHeader";
+import Intro from "./Intro";
 
 
 export default function About() {
-    return(
-        <div className='w-full h-full '>
-            
+    return (
+        <div className='w-[70%] h-[82%] absolute left-[15%] top-[8%] border-2 border-indigo-200/70 z-30 rounded-2xl overflow-hidden'>
+            <AppWindowHeader name='About Me' />
+
+            <div className='bg-[#0b1544]  h-full w-full'>
+
+                <Intro/>
+
+            </div>
+
         </div>
     )
 }
